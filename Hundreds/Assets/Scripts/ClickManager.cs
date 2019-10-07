78391959f357851b1cf27e1849336dbc52becdde
Cooper Wallace <CooperWallace@users.noreply.github.com>
2019-10-07 12:24:08 -0600

    Refactor Input to a single file

--- Hundreds/Assets/Scripts/ClickManager.cs
+++ Hundreds/Assets/Scripts/ClickManager.cs
@@ -3,58 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/* This class is dedicated to handling Mouse Clicks and Multiple Touch Screen
+ * touches upon the Balls in a Scene
+ *
+ *
+ */
+
 public class ClickManager : MonoBehaviour
 {
+	public GameObject TotalPoints;		// Point text associated with object
+	private float growthRate;
 
-    public GameObject TotalPoints;
+	// Start is called before the first frame update
+	void Start()
+	{
+		growthRate = Camera.main.orthographicSize * 2 * 1.0f / 100;
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		// Handle Growth based upon Mouse Click
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if (Input.GetMouseButton(0)){
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+		if (Input.GetMouseButton(0)){
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
 
-            if (hit.collider != null)
-            {
-                Debug.Log("something was clicked!");
-                if (hit.collider.gameObject.name == "Sphere(Clone)")
-                {
-                    Debug.Log(hit.collider.gameObject.name);
-                    hit.collider.attachedRigidbody.AddForce(Vector2.up*0);
+			if (hit.collider != null && hit.collider.gameObject.name == "Sphere(Clone)")
+			{
+				Debug.Log("something was clicked!");
+				growObject(hit);
+			}
+			Debug.Log("Mouse Clicked");
+		}
 
-                    int totalPoints = int.Parse(TotalPoints.GetComponent<TextMeshPro>().text);
+		// Handle growth for Touch Screen touches
+		// Note: If touch is 0, then we never enter the loop
+		for (int i = 0; i < Input.touchCount; i++)
+		{
+			Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+			Vector2 touchPos2D = new Vector2(touchPos.x, touchPos.y);
 
-                    // Only grow an object if it still fits the screen, and if totalpoints < 100
-                    if ((hit.collider.gameObject.transform.localScale[1] < Camera.main.orthographicSize * 2)
-                        && (totalPoints < 100))
-                    {
-                        // Grow object by increment
-                        float increment = Camera.main.orthographicSize * 2 * 1.0f / 100;
-                        hit.collider.gameObject.transform.localScale += new Vector3(increment, increment, increment);
+			RaycastHit2D hit = Physics2D.Raycast(touchPos2D, Vector2.zero);
 
-                        // Add a point to the ball
-                        GameObject pointsText = hit.collider.gameObject.transform.Find("PointsText(Clone)").gameObject;
-                        int points = int.Parse(pointsText.GetComponent<TextMeshPro>().text);
-                        points += 1;
-                        pointsText.GetComponent<TextMeshPro>().text = points.ToString();
+			if (hit.collider != null && hit.collider.gameObject.name == "Sphere(Clone)")
+			{
+				hit.collider.gameObject.transform.localScale += new Vector3(growthRate, growthRate, 0);
+			}
+		}
 
-                        // Add a point to total points
-                        totalPoints += 1;
-                        TotalPoints.GetComponent<TextMeshPro>().text = totalPoints.ToString();
-                    }
-                }
-            }
-            Debug.Log("Mouse Clicked");
-        }
-    }
+	}
+
+	// Grow the Sphere Object
+	void growObject(RaycastHit2D hit)
+	{
+		int totalPoints = int.Parse(TotalPoints.GetComponent<TextMeshPro>().text);
+
+		// Only grow an object if it still fits the screen, and if totalpoints < 100
+		if ((hit.collider.gameObject.transform.localScale[1] < Camera.main.orthographicSize * 2)
+				&& (totalPoints < 100))
+		{
+			// Grow object by increment
+			hit.collider.gameObject.transform.localScale += new Vector3(growthRate, growthRate, growthRate);
+
+			// Add a point to the ball
+			GameObject pointsText = hit.collider.gameObject.transform.Find("PointsText(Clone)").gameObject;
+			int points = int.Parse(pointsText.GetComponent<TextMeshPro>().text);
+			points += 1;
+			pointsText.GetComponent<TextMeshPro>().text = points.ToString();
+
+			// Add a point to total points
+			totalPoints += 1;
+			TotalPoints.GetComponent<TextMeshPro>().text = totalPoints.ToString();
+		}
+	}
+
+
 }
