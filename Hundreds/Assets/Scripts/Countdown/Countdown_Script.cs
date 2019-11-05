39db9e09bfab35a60896e26d8239c4d22a77f35d
Wamaitha <gitakan@mymacewan.ca>
2019-11-05 13:40:34 -0700

    New Countdown and Change in Menu

---
+++ Hundreds/Assets/Scripts/Countdown/Countdown_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Countdown_Script : MonoBehaviour
+{
+
+    public float countdownTime;
+    private TextMeshPro countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownText = GetComponent<TextMeshPro>();
+        countdownText.text = countdownTime.ToString("0");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When the timer reaches 0, switch to endless mode 
+        if (countdownTime <= 0)
+        {
+            SceneManager.LoadScene("Endless Mode");
+        }
+
+        countdownTime -= Time.deltaTime;
+        countdownText.text = countdownTime.ToString("0");
+    }
+}
+
