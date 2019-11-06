d8980aa8125e9fda3f826dac75718c9a58bbbdbd
Cooper Wallace <CooperWallace@users.noreply.github.com>
2019-11-06 13:44:46 -0700

    Add Pause Buttons to each corner of the Screen.
    Each Pause button will pause the game after a specified time delay is
    reached.

---
+++ Hundreds/Assets/Scripts/GameScripts/SpawnPauseButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Spawns 4 Pause Button prefabs on each side of the Gameplay screen.  */
+
+public class SpawnPauseButtons : MonoBehaviour
+{
+	private Camera cam;
+	public GameObject PauseButtonPrefab;
+	public float PauseDelay;
+	public PauseManager PauseManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		cam = Camera.main;
+
+		// Spawn Buttons on each sides of the screen
+		for (int i = 0; i < 2; i++) {
+			for (int d = 0; d < 2; d++) {
+				GameObject n = Instantiate(PauseButtonPrefab,
+						cam.ViewportToWorldPoint(new Vector3(i,d,1)),
+						Quaternion.identity);
+				// Set a Reference to the PauseManager so the game can be paused
+				GameplayPauseScript gps = n.GetComponent<GameplayPauseScript>();
+				gps.SetPauseDelay(PauseDelay);
+				gps.SetPauseManager(PauseManager);
+			}
+		}
+    }
+}
---
+++ Hundreds/Assets/Scripts/Menu/GameplayPauseScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Gameplay will be paused after a PauseButton object is help for a specific
+ * amount of time.
+ */
+public class GameplayPauseScript : MonoBehaviour
+{
+	private float clickedTime;
+	private bool isClicked;
+	private PauseManager pauseManager;
+	private float PauseDelay;
+
+	void Start()
+	{
+		enabled = false;
+		clickedTime = 0f;
+	}
+
+	// While enabled, count how long the pause button has been pressed.
+	void Update()
+	{
+		if (GameManager.isGamePaused())
+			return;
+
+		clickedTime += Time.deltaTime;
+
+		if (clickedTime >= PauseDelay) {
+			pauseManager.TogglePauseMenu();
+			enabled = false;
+		}
+	}
+	/* Enable Update method to start counting, and disable to reset */
+
+    void OnMouseDown()
+    {
+		enabled = true;
+    }
+
+
+	void OnMouseUp()
+	{
+		clickedTime = 0f;
+		enabled = false;
+	}
+
+	// Set Reference to PauseManager so that we may pause the game.
+	public void SetPauseManager(PauseManager p) { pauseManager = p; }
+
+	// Set the delay before the game is paused
+	public void SetPauseDelay(float delay) { PauseDelay = delay; }
+}
