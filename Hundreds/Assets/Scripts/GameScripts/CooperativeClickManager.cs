5c9cc71453be7e3de33bda05cc16ce86be49ef5d
Adam Reeves <reevesa6@macewan.ca>
2019-11-21 17:38:26 -0700

    Add a colour setting function and function to only grow balls when two or more balls of the same colour are touched

---
+++ Hundreds/Assets/Scripts/GameScripts/CooperativeClickManager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/* This class is dedicated to handling Mouse Clicks and Multiple Touch Screen
+ * touches upon the Balls in a Scene
+ */
+
+public class CooperativeClickManager : MonoBehaviour
+{
+    public GameObject TotalPoints;      // Point text associated with object
+    public WinLoseManager WLM;
+    public CooperativeSpawnBalls CSP;
+    private float growthRate;
+    private int totalPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        growthRate = Camera.main.orthographicSize * 2 * 1.0f / 100;
+
+        // Initialize Total Points to 0
+        TotalPoints.GetComponent<TextMeshPro>().text = "0";
+        totalPoints = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ignore all Mouse Clicks if the game is paused
+        if (GameManager.isGamePaused())
+            return;
+
+        // If totalPoints is 100, go to Win Menu
+        if (totalPoints == 100)
+            WLM.GetWinMenu();
+
+        Debug.Log("we living");
+        // Get All Collided gameobjects and apply function based on type
+        Debug.Log(CSP);
+        HashSet<GameObject>[] colourGroups = CSP.GroupColours(GetInputCollisions());
+        foreach (HashSet<GameObject> colourGroup in colourGroups)
+        {
+            if (colourGroup.Count>=2){
+                foreach (GameObject entry in colourGroup)
+                {
+                    growSphereObject(entry);
+                }
+            }
+        }
+        //foreach (GameObject entry in GetInputCollisions())
+        //{
+        //    if (entry.name == "Sphere(Clone)")
+        //        growSphereObject(entry);
+        //    else if (entry.name == "PauseButton(Clone)")
+        //    {
+        //        entry.GetComponent<GameplayPauseScript>().setClicked();
+
+    }    
+
+    // Handles both the Mouse and Touch Inputs upon objects, and returns all
+    // objects in a HashSet
+    HashSet<GameObject> GetInputCollisions()
+    {
+        // Store all Collisions in a set to prevent duplication
+        HashSet<GameObject> FrameCollisions = new HashSet<GameObject>();
+
+        // If there is no Touch points, default to mouse
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            GameObject collisionObj = getCollidedGameObject(Input.touches[i].position);
+            if (collisionObj)
+                FrameCollisions.Add(collisionObj);
+        }
+
+        // Handle Mouse Input if there is any
+        if (Input.GetMouseButton(0))
+        {
+            GameObject collisionObj = getCollidedGameObject(Input.mousePosition);
+            if (collisionObj)
+                FrameCollisions.Add(collisionObj);
+        }
+
+        return FrameCollisions;
+    }
+
+    // Get the Game Object that the provided input vector collides with
+    GameObject getCollidedGameObject(Vector3 position)
+    {
+        // Implicit conversion from Vector3 to Vector2
+        Vector2 inputPos = Camera.main.ScreenToWorldPoint(position);
+        RaycastHit2D hit = Physics2D.Raycast(inputPos, Vector2.zero);
+
+        if (hit.collider)
+            return hit.collider.gameObject;
+
+        return null;
+    }
+
+
+    // Grow the Sphere Object
+    void growSphereObject(GameObject sphere)
+    {
+        if (sphere.GetComponent<SphereObject>().collided)
+        {
+            WLM.GetLoseMenu();
+            return;
+        }
+
+        // Only grow an object if it still fits the screen, and if totalpoints < 100
+        if ((sphere.transform.localScale[1] < Camera.main.orthographicSize * 2))
+        {
+            // Grow object by increment
+            sphere.transform.localScale += new Vector3(growthRate, growthRate, growthRate);
+
+            // Add a point to the ball
+            GameObject pointsText = sphere.transform.Find("PointsText(Clone)").gameObject;
+            int points = int.Parse(pointsText.GetComponent<TextMeshPro>().text);
+            points += 1;
+            pointsText.GetComponent<TextMeshPro>().text = points.ToString();
+
+            // Add a point to total points
+            totalPoints += 1;
+            TotalPoints.GetComponent<TextMeshPro>().text = totalPoints.ToString();
+        }
+    }
+
+
+}
---
+++ Hundreds/Assets/Scripts/GameScripts/CooperativeSpawnBalls.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class is responsible for spawning a predefined number of balls when
+// loading the play scene.
+public class CooperativeSpawnBalls : MonoBehaviour
+{
+    public GameObject BallPrefab;   // Prefab of the Ball
+    [Tooltip("Initial Number of Spheres (Level 1)")]
+    public int InitialSpawnNumber;
+    [Tooltip("Maximum number of Spheres to spawn")]
+    public int SpawnCap;
+    [Tooltip("Initial Velocity of the Spheres (Level 1)")]
+    public int BaseVelocity;
+    [Tooltip("Rate Scaler for the Velocity of Objects to change each level")]
+    public float VelocityScaler;
+    public int GroupSize;
+    [Tooltip("number of balls of the same colour")]
+    private Camera cam;
+    public Color[] colours;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        spawnObjects();
+
+        if (GameManager.isGamePaused())
+            GameManager.TogglePause();
+    }
+
+    // Return a random Vector2 that is within 0.05f of the edges of the screen.
+    private Vector3 getRandomPoint()
+    {
+        float x = Random.Range(0.05f, 0.95f);
+        float y = Random.Range(0.05f, 0.95f);
+
+        Vector3 pt = new Vector3(x, y, 0);
+        return cam.ViewportToWorldPoint(pt);
+    }
+
+    /* Spawn a number of Sphere objects based upon the level of the game.
+	 * First level will be 4, and increments until Max is reached
+	 */
+    private void spawnObjects()
+    {
+        int SpawnNumber = InitialSpawnNumber + (GameManager.GetGameLevel() - 1);
+        if (SpawnNumber > SpawnCap)
+            SpawnNumber = SpawnCap;
+
+        // Change Velocity based upon Game Level and Velocity Scaler
+        float MaxBallVelocity = BaseVelocity;
+        MaxBallVelocity += GameManager.GetGameLevel() * VelocityScaler;
+
+        // Instantiate SpawnNumber of Ball Prefabs in the Scene
+        for (int i = 0; i < SpawnNumber; i++)
+        {
+            GameObject c = Instantiate(BallPrefab, getRandomPoint(), Quaternion.identity) as GameObject;
+            //c.GetComponent<Renderer>().material.color = new Color(255f, 0,0);
+            assignColours(c, i, SpawnNumber);
+            // Change Object options based upon the level
+            var StartScript = c.GetComponent<SphereObject>();
+            StartScript.SetMaximumVelocity(MaxBallVelocity);
+        }
+    }
+    private void assignColours(GameObject ball, int ballNum, int SpawnNumber)
+    {
+        //Color[] colours = new Color[4]{new Color(255f,0,0),new Color(0,255f,0),new Color(0,0,255f) };
+        //Color[] colours = { new Color(255f, 0, 0), new Color(0, 255f, 0), new Color(0, 0, 255f),new Color(255f,255f,255f)};
+        if (ballNum >= (SpawnNumber - (SpawnNumber % GroupSize)))
+        {
+            ball.GetComponent<Renderer>().material.color = colours[ballNum % GroupSize];
+        }
+        else
+        {
+            ball.GetComponent<Renderer>().material.color = colours[ballNum / GroupSize];
+        }
+    }
+    public HashSet<GameObject>[] GroupColours(HashSet<GameObject> balls)
+    {
+        int i=0;
+        HashSet<GameObject>[] colourGroups = new HashSet<GameObject>[colours.Length];
+        for (i = 0; i < colours.Length; i++)
+        {
+            colourGroups[i] = new HashSet<GameObject>();
+        }
+        foreach (GameObject ball in balls)
+        {
+            i = 0;
+            foreach (Color colour in colours)
+            {
+                if (ball.GetComponent<Renderer>().material.color == colour)
+                {
+                    colourGroups[i].Add(ball);
+                }
+                i++;
+            }
+        }
+        return colourGroups;
+    }
+
+}
